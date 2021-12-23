87524435911d03384c69aebe0a7db8e09b8a854d
Random Looq <47354933+rlooq@users.noreply.github.com>
2021-12-23 23:13:56 +0100

    added credentials for Twitter API

---
+++ credentials.py
@@ -0,0 +1,7 @@
+cfg = {
+    "bearer_token": "Your bearer token",
+    "consumer_key": "Your consumer key",
+    "consumer_secret": "Your consumer secret",
+    "access_token": "Your access token",
+    "access_token_secret": "Your access token secret",
+}
